fb7121146cf8b69f03a723adba1caa96d97ceabd
Cormac <csmith715@gmail.com>
2024-03-27 19:49:34 -0400

    First Commit

---
+++ excel_modification.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import openpyxl
+import re
+import glob
+from datetime import datetime
+import streamlit as st
+from io import BytesIO
+from base64 import b64encode
+
+class FileUpdate:
+    def __init__(self):
+        # self.base_directory = base_dir_
+        self.workbook = openpyxl.load_workbook('data/FE Template - WFAS - CLIENT - BORROWER - Field Exam Report - Automation.xlsx')
+        self.cols = ['Payer Type', 'Future Cash', 'Current', '30', '60', '90', '120', '150', '180', '210', 'As Of Date']
+        self.num_payers = 1
+        self.facility_cols = {
+            1: ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'],
+            2: ['Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF'],
+            3: ['AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ'],
+            4: ['AU', 'AV', 'AW', 'AX', 'AY', 'AZ', 'BA', 'BB'],
+            5: ['BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM']
+        }
+        self.facility_name_cells = {1: 'M14', 2: 'X14', 3: 'AI14', 4: 'AT14', 5: 'BE14', 6: 'BP14', 7: 'CA14', 8: 'CL14'}
+        self.facility_cell = ''
+        self.facility_cols_single_file = ['O', 'P', 'Q', 'R', 'S', 'T', 'U']
+        self.sheet_cols = []
+        self.ar_bucket_labels = ['30', '60', '90', '120', '150', '180']
+        self.ar_bucket_ranges = [
+            range(1, 31),
+            range(31, 61),
+            range(61, 91),
+            range(91, 121),
+            range(121, 151),
+            range(151, 181)
+        ]
+        self.date_start_row = 81
+        self.unique_payers = []
+        self.payer_mappings = {'Medicare A': 'Medicare', 'Medicare B': 'Medicare'}
+        self.max_date = None
+
+    def update_facilities(self, base_dir: str, sub_directories: list):
+        for i, sub in enumerate(sub_directories, start=1):
+            self.sheet_cols = self.facility_cols[i]
+            self.facility_cell = self.facility_name_cells[i]
+            path = f'{base_dir}/{sub}/*.csv'
+            glob_files = glob.glob(path)
+            dataframes = []
+            for file in glob_files:
+                dframe = pd.read_csv(file, usecols=self.cols).fillna('')
+                dataframes.append(dframe)
+            temp_df = pd.concat(dataframes).reset_index(drop=True)
+            temp_df['30'] = temp_df['Current'] + temp_df['30']
+            temp_df = temp_df.drop('Current', axis=1)
+            temp_df = temp_df[temp_df['Payer Type'] != ''].rename(columns={'As Of Date': 'Date'})
+            temp_df['Date'] = [datetime.strptime(w, '%m/%d/%Y') for w in temp_df.Date]
+            temp_df['Payer Type'] = temp_df['Payer Type'].replace(self.payer_mappings)
+            self.date_start_row = (24 - len(pd.unique(temp_df.Date))) + 81
+            self.unique_payers = temp_df['Payer Type'].unique()
+            # print(self.unique_payers)
+            self.num_payers = len(self.unique_payers)
+            self.update_facility(temp_df, sub, False)
+        # self.workbook.save('multi_file_test.xlsx')
+        output = BytesIO()
+        self.workbook.save(output)
+        xlsx_data = output.getvalue()
+        output.seek(0)
+
+        # Generate download link
+        href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64encode(xlsx_data).decode()}" download="Modified.xlsx">Download Excel File</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
+    def update_facility(self, df, facility_name, single=False):
+        if single:
+            df_grouped = df
+            self.sheet_cols = self.facility_cols_single_file
+            self.unique_payers = pd.unique(df.index.get_level_values('Payer Type'))
+            self.facility_cell = 'M14'
+        else:
+            df_grouped = df.groupby(['Payer Type', 'Date']).sum()
+            self.max_date = max(df['Date'])
+            self.unique_payers.sort()
+        ar6 = self.workbook['AR6  AR Aging Trend']
+        ar6['A39'] = self.max_date
+        ar6[self.facility_cell] = re.sub('\d - ', '', facility_name)
+        ranges = [self.date_start_row+(x*33) for x in range(self.num_payers)]
+        payer_name_start_cell = [f'A{(r*33)+79}' for r in range(self.num_payers)]
+        for w, start_row_val, pcell in zip(self.unique_payers, ranges, payer_name_start_cell):
+            payor_df = df_grouped[df_grouped.index.get_level_values('Payer Type') == w]
+            ar6[pcell] = w
+            row_sums = [row for row in payor_df.values]
+            for r, row_add in zip(row_sums, range(len(row_sums))):
+                for row_val, col in zip(r, self.sheet_cols):
+                    ar6[f'{col}{start_row_val+row_add}'] = row_val
+
+    def update_single_facility(self, facility_file, facility_name):
+        single_df = self.process_single_file(facility_file)
+        self.unique_payers = pd.unique(single_df.index.get_level_values('Payer Type'))
+        self.num_payers = len(self.unique_payers)
+        self.update_facility(single_df, facility_name, True)
+        output = BytesIO()
+        self.workbook.save(output)
+        xlsx_data = output.getvalue()
+        output.seek(0)
+
+        # Generate download link
+        suffix = '<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,'
+        href = f'{suffix}{b64encode(xlsx_data).decode()}" download="Modified.xlsx">Download Excel File</a>'
+        st.markdown(href, unsafe_allow_html=True)
+        return single_df
+
+    def label_ar_bucket(self, value: int):
+        label = ''
+        if value > 180:
+            label = 'Over 180'
+        else:
+            for ar_range, lab in zip(self.ar_bucket_ranges, self.ar_bucket_labels):
+                if value in ar_range:
+                    label = lab
+        return label
+
+    def process_single_file(self, single_file: str):
+        sdf = pd.ExcelFile(single_file)
+        sheet_names = sdf.sheet_names
+        sheet_dates = [s.strip('ATB ').replace('  ', ' ') for s in sheet_names]
+        sheet_dates = [datetime.strptime(d, '%m %d %Y') for d in sheet_dates]
+        self.max_date = max(sheet_dates)
+        self.date_start_row = (24 - len(sheet_dates)) + 81
+        all_dataframes = []
+        for tab_name in sheet_names:
+            tab_df = pd.read_excel(single_file, sheet_name=tab_name)
+            tab_df = tab_df.rename(columns={
+                'FIN CLASS': 'Financial Class',
+                'FC': 'Financial Class',
+                'DISCH DATE': 'DISCH DT',
+                ' ACHGS ': 'ACHGS'
+            })
+            sdate = datetime.strptime(tab_name.strip('ATB ').replace('  ', ' '), '%m %d %Y')
+            days_since_discharge = [(sdate - d).days for d in tab_df['DISCH DT']]
+            ar_buckets = [self.label_ar_bucket(max(1, d)) for d in days_since_discharge]
+            temp_df = pd.DataFrame({
+                'Payer Type': tab_df['Financial Class'],
+                'Charges': tab_df['ACHGS'],
+                'AR Bucket': ar_buckets,
+                'Date': sdate
+            })
+            all_dataframes.append(temp_df)
+        all_tab_df = pd.concat(all_dataframes).reset_index(drop=True)
+        pivot_df = all_tab_df.pivot_table(values='Charges', index=['Payer Type', 'Date'], columns='AR Bucket', aggfunc='sum')
+        pivot_df = pivot_df[['30', '60', '90', '120', '150', '180', 'Over 180']].sort_index()
+        return pivot_df
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,71 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-import altair as alt
+from excel_modification import FileUpdate
 
 # Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+st.set_page_config(page_title='West-Fork Data Explorer', page_icon='📊')
+st.title('West-Fork Data Exploration')
+st.markdown('## Steps to use:\n\n1. Select and load single tabbed Excel file\n2. Enter the facility name\n3. Click "Process File"')
+
+file_update = FileUpdate()
+
+uploaded_file = st.file_uploader("Upload a single Excel file", type=["xls", "xlsx"])
+
+# directory_path = st.text_input('Enter directory path:')
+# facility_names = st.text_input('List the names of the facilities/folders')
+single_facility_name = st.text_input('Enter the name of the facility')
+
+single_df = pd.DataFrame()
+if st.button('Process File(s)'):
+    if not single_facility_name:
+        st.markdown('Please add a file name')
+    if uploaded_file and single_facility_name:
+        single_df = file_update.update_single_facility(uploaded_file, single_facility_name)
+    # elif facility_names is not None and directory_path is not None:
+    #     dir_names = facility_names.split(',')
+    #     file_update.update_facilities(directory_path, dir_names)
+
+
+# with st.expander('About this app'):
+#   st.markdown('**What can this app do?**')
+#   st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
+#   st.markdown('**How to use the app?**')
+#   st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget.
+#   As a result, this should generate an updated editable DataFrame and line plot.')
   
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+# st.subheader('Which Movie Genre performs ($) best at the box office?')
 
 # Load data
-df = pd.read_csv('data/movies_genres_summary.csv')
-df.year = df.year.astype('int')
+# df = pd.read_csv('data/movies_genres_summary.csv')
+# df.year = df.year.astype('int')
 
 # Input widgets
-## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+# Genres selection
+# genres_list = df.genre.unique()
+# genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+#
+# ## Year selection
+# year_list = df.year.unique()
+# year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
+# year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
 
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
+# df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
+# reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
+# reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
 
 
 # Display DataFrame
+# Un-pivot dataframe
+udf = single_df.reset_index()
+df_editor = st.data_editor(udf, height=300, use_container_width=True, num_rows="dynamic")
 
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
-                            num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
+# df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
 
-# Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+# # Display chart
+# chart = alt.Chart(df_chart).mark_line().encode(
+#             x=alt.X('year:N', title='Year'),
+#             y=alt.Y('gross:Q', title='Gross earnings ($)'),
+#             color='genre:N'
+#             ).properties(height=320)
+# st.altair_chart(chart, use_container_width=True)
