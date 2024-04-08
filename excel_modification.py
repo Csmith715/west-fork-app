91cc52cbd2fa575ad46ee8325c9f9ee596b6e201
Cormac <csmith715@gmail.com>
2024-04-08 11:03:39 -0400

    Added zip file functionality

--- excel_modification.py
+++ excel_modification.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import openpyxl
 import re
-import glob
 from datetime import datetime
 import streamlit as st
+import io
 from io import BytesIO
+import zipfile
 from base64 import b64encode
 
 class FileUpdate:
@@ -37,37 +38,53 @@ class FileUpdate:
         self.unique_payers = []
         self.payer_mappings = {'Medicare A': 'Medicare', 'Medicare B': 'Medicare'}
         self.max_date = None
+        self.encoding_options = ['utf-8', 'latin1', 'ISO-8859-1']
 
-    def update_facilities(self, base_dir: str, sub_directories: list):
-        for i, sub in enumerate(sub_directories, start=1):
-            self.sheet_cols = self.facility_cols[i]
-            self.facility_cell = self.facility_name_cells[i]
-            path = f'{base_dir}/{sub}/*.csv'
-            glob_files = glob.glob(path)
-            dataframes = []
-            for file in glob_files:
-                dframe = pd.read_csv(file, usecols=self.cols).fillna('')
-                dataframes.append(dframe)
-            temp_df = pd.concat(dataframes).reset_index(drop=True)
-            temp_df['30'] = temp_df['Current'] + temp_df['30']
-            temp_df = temp_df.drop('Current', axis=1)
-            temp_df = temp_df[temp_df['Payer Type'] != ''].rename(columns={'As Of Date': 'Date'})
-            temp_df['Date'] = [datetime.strptime(w, '%m/%d/%Y') for w in temp_df.Date]
-            temp_df['Payer Type'] = temp_df['Payer Type'].replace(self.payer_mappings)
-            self.date_start_row = (24 - len(pd.unique(temp_df.Date))) + 81
-            self.unique_payers = temp_df['Payer Type'].unique()
-            # print(self.unique_payers)
-            self.num_payers = len(self.unique_payers)
-            self.update_facility(temp_df, sub, False)
-        # self.workbook.save('multi_file_test.xlsx')
-        output = BytesIO()
-        self.workbook.save(output)
-        xlsx_data = output.getvalue()
-        output.seek(0)
+    def decode_file(self, file) -> pd.DataFrame:
+        for encoding in self.encoding_options:
+            try:
+                dframe = pd.read_csv(io.StringIO(file.decode(encoding))).fillna('')
+                return dframe
+            except UnicodeDecodeError:
+                pass
 
-        # Generate download link
-        href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64encode(xlsx_data).decode()}" download="Modified.xlsx">Download Excel File</a>'
-        st.markdown(href, unsafe_allow_html=True)
+    def update_facilities(self, zip_file):
+        zip_dict = {}
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            csv_files = [file.filename for file in zip_ref.infolist() if file.filename.endswith('.csv')]
+            for file in csv_files:
+                key_name = file.split('/')[1]
+                key_name = re.sub('\d - ', '', key_name)
+                with zip_ref.open(file) as cfile:
+                    content = cfile.read()
+                    content_df = self.decode_file(content)
+                if content_df.shape[1] > 10:
+                    if key_name not in zip_dict.keys():
+                        zip_dict[key_name] = [content_df]
+                    else:
+                        zip_dict[key_name].extend([content_df])
+            final_dfs = []
+            for i, (sub, facility_dfs) in enumerate(zip_dict.items(), start=1):
+                self.sheet_cols = self.facility_cols[i]
+                self.facility_cell = self.facility_name_cells[i]
+                # Concatenate dataframes
+                temp_df = pd.concat(facility_dfs)
+                temp_df = temp_df[self.cols].fillna('')
+                temp_df['30'] = temp_df['Current'] + temp_df['30']
+                temp_df = temp_df.drop('Current', axis=1)
+                temp_df = temp_df[temp_df['Payer Type'] != ''].rename(columns={'As Of Date': 'Date'})
+                temp_df = temp_df.reset_index(drop=True)
+                temp_df['Date'] = [datetime.strptime(w, '%m/%d/%Y') for w in temp_df.Date]
+                temp_df['Payer Type'] = temp_df['Payer Type'].replace(self.payer_mappings)
+                self.date_start_row = (24 - len(pd.unique(temp_df.Date))) + 81
+                self.unique_payers = temp_df['Payer Type'].unique()
+                self.num_payers = len(self.unique_payers)
+                self.update_facility(temp_df, sub, False)
+                final_dfs.append(temp_df)
+            self.save_file()
+            final_df = pd.concat(final_dfs)
+            final_df = final_df.groupby(['Payer Type', 'Date']).sum()
+            return final_df
 
     def update_facility(self, df, facility_name, single=False):
         if single:
@@ -81,7 +98,7 @@ class FileUpdate:
             self.unique_payers.sort()
         ar6 = self.workbook['AR6  AR Aging Trend']
         ar6['A39'] = self.max_date
-        ar6[self.facility_cell] = re.sub('\d - ', '', facility_name)
+        ar6[self.facility_cell] = facility_name
         ranges = [self.date_start_row+(x*33) for x in range(self.num_payers)]
         payer_name_start_cell = [f'A{(r*33)+79}' for r in range(self.num_payers)]
         for w, start_row_val, pcell in zip(self.unique_payers, ranges, payer_name_start_cell):
@@ -97,6 +114,10 @@ class FileUpdate:
         self.unique_payers = pd.unique(single_df.index.get_level_values('Payer Type'))
         self.num_payers = len(self.unique_payers)
         self.update_facility(single_df, facility_name, True)
+        self.save_file()
+        return single_df
+
+    def save_file(self):
         output = BytesIO()
         self.workbook.save(output)
         xlsx_data = output.getvalue()
@@ -106,7 +127,6 @@ class FileUpdate:
         suffix = '<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,'
         href = f'{suffix}{b64encode(xlsx_data).decode()}" download="Modified.xlsx">Download Excel File</a>'
         st.markdown(href, unsafe_allow_html=True)
-        return single_df
 
     def label_ar_bucket(self, value: int):
         label = ''
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,54 +6,28 @@ from excel_modification import FileUpdate
 
 st.set_page_config(page_title='West-Fork Data Explorer', page_icon='📊')
 st.title('West-Fork Data Exploration')
-st.markdown('## Steps to use:\n\n1. Select and load single tabbed Excel file\n2. Enter the facility name\n3. Click "Process File"')
+st.markdown('## Single File Processing')
+st.markdown('### Steps to use:\n\n1. Select and load single tabbed Excel file\n2. Enter the facility name\n3. Click "Process File"')
 
 file_update = FileUpdate()
 
 uploaded_file = st.file_uploader("Upload a single Excel file", type=["xls", "xlsx"])
 
-# directory_path = st.text_input('Enter directory path:')
-# facility_names = st.text_input('List the names of the facilities/folders')
 single_facility_name = st.text_input('Enter the name of the facility')
 
 single_df = pd.DataFrame()
-if st.button('Process File(s)'):
+if st.button('Process Single File'):
     if not single_facility_name:
         st.markdown('Please add a file name')
     if uploaded_file and single_facility_name:
         single_df = file_update.update_single_facility(uploaded_file, single_facility_name)
-    # elif facility_names is not None and directory_path is not None:
-    #     dir_names = facility_names.split(',')
-    #     file_update.update_facilities(directory_path, dir_names)
-
-
-# with st.expander('About this app'):
-#   st.markdown('**What can this app do?**')
-#   st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-#   st.markdown('**How to use the app?**')
-#   st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget.
-#   As a result, this should generate an updated editable DataFrame and line plot.')
-  
-# st.subheader('Which Movie Genre performs ($) best at the box office?')
-
-# Load data
-# df = pd.read_csv('data/movies_genres_summary.csv')
-# df.year = df.year.astype('int')
-
-# Input widgets
-# Genres selection
-# genres_list = df.genre.unique()
-# genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
-#
-# ## Year selection
-# year_list = df.year.unique()
-# year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-# year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-# df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-# reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-# reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
-
+st.divider()
+st.markdown('\n\n## Multiple File Processing')
+st.markdown('### Steps to use:\n\n1. Select and load single zipped file of the main folder containing the facilities in the sub folders (i.e., Example 1)\n2. Click "Process File"')
+zip_file = st.file_uploader("Upload a single Zip file", type=["zip"])
+if st.button('Process Zip File'):
+    if zip_file:
+        single_df = file_update.update_facilities(zip_file)
 
 # Display DataFrame
 # Un-pivot dataframe
